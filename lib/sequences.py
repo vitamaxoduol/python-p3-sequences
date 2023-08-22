c0670d52cf195a36e33bc8f7e8afc7978ea78e4f
Vitamax <vitamaxoduo@gmail.com>
2023-08-22 12:07:24 +0300

    sequences in python

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
+# list = [0, 1, 1, 2, 3, 5, 8, 13, 21]
+
 def print_fibonacci(length):
-    pass
+    a, b = 0, 1
+    fibonacci = []
+    
+    for _ in range(length):
+        fibonacci.append(a)
+        a, b = b, a + b
+    
+    print(fibonacci)
+        
+print(print_fibonacci(9)) 
+    
